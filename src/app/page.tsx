5c365d3920042d4b785a41c2afbc9d3d3e344d76
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2026-01-19 18:18:23 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-red-500">Home</h1>
+      <Button>Bootcamp!</Button>
     </div>
   );
 };
-
+ 
 export default Home;
