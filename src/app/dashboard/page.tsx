edfbc2500f324fd0bd802c16449a19de42949e26
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2026-01-22 15:00:16 -0300

    feat: implement authentication flow with session management and sign out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
-const DashboardPage = () => {
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+import { auth } from '@/lib/auth';
+
+import SignOutButton from './components/sign-out-button';
+
+const DashboardPage = async () => {
+  // Cria sessao do Better-auth
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // redireciona para o authentication se nao tiver autenticado.
+  if (!session?.user) {
+    redirect('/authentication');
+  }
   return (
     <div>
-      <h1>DashboardPage</h1>
+      <h1>Dashboard</h1>
+      <h1>{session?.user.name}</h1>
+      <h1>{session?.user.email}</h1>
+      <SignOutButton />
     </div>
   );
 };
