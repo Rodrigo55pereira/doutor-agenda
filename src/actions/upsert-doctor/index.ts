77c70d457fe992d2fbdeed109e7ee02bd7f6e32b
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2026-01-29 10:23:57 -0300

    feat: ensure doctor availability is saved as utc on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 import { headers } from 'next/headers';
 
 import { db } from '@/db';
@@ -9,9 +11,26 @@ import { actionClient } from '@/lib/next-safe-action';
 
 import { upsertDoctorSchema } from './schema';
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime;
+    const availableToTime = parsedInput.availableToTime;
+
+    const availableFromTimeUTC = dayjs()
+      .set('hour', parseInt(availableFromTime.split(':')[0])) // [15, 30, 00]
+      .set('minute', parseInt(availableFromTime.split(':')[1]))
+      .set('second', parseInt(availableFromTime.split(':')[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set('hour', parseInt(availableToTime.split(':')[0])) // [15, 30, 00]
+      .set('minute', parseInt(availableToTime.split(':')[1]))
+      .set('second', parseInt(availableToTime.split(':')[2]))
+      .utc();
+
     // pega dados da sessao e do banco de dados conforme necessário.
     const session = await auth.api.getSession({
       headers: await headers(),
@@ -32,12 +51,16 @@ export const upsertDoctor = actionClient
       .values({
         ...parsedInput,
         clinicId: session?.user.clinic?.id,
+        availableFromTime: availableFromTimeUTC.format('HH:mm:ss'),
+        availableToTime: availableToTimeUTC.format('HH:mm:ss'),
       })
       // Se o ID já existir, atualiza o registro existente
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...parsedInput,
+          availableFromTime: availableFromTimeUTC.format('HH:mm:ss'),
+          availableToTime: availableToTimeUTC.format('HH:mm:ss'),
         },
       });
   });
